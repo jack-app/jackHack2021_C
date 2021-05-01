cfcfd2b4bcc28d05dc7f2370f35316ab3e442eeb
shin04 <daikon0413@gmail.com>
2021-05-01 21:21:46 +0900

    db connection

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 import os
 
+from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
 
+from db import db
 from utils.error_handler import handle_bad_request, handle_not_found_request
 from controller.hello import hello
 
@@ -18,5 +20,17 @@ app.add_url_rule('/hello', 'hello', hello)
 
 
 if __name__ == "__main__":
+    load_dotenv()
+    db_config = {
+        'user': os.getenv('DB_USER'),
+        'password': os.getenv('DB_PASS'),
+        'host': os.getenv('DB_HOST'),
+        'db_name': os.getenv('DB_NAME')
+    }
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        'mysql+pymysql://{user}:{password}@{host}/{db_name} \
+        ?charset=utf8'.format(**db_config)
+    db.init_db(app)
+
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
