d532b9b54eb620a2967f0bb0d2eeb9515a1209b7
shin04 <daikon0413@gmail.com>
2021-05-02 23:32:33 +0900

    check topic API

--- backend/app.py
+++ backend/app.py
@@ -10,6 +10,7 @@ from controller.hello import get_hello, post_hello
 from controller.user import get_users, get_user, register_user
 from controller.diary import get_diarys, get_diary, register_diary
 from controller.situation_type import get_situation_type, resister_situation_type
+from controller.topic import get_topics, register_topic
 from controller.extract_words import extract_words
 
 app = Flask(__name__)
@@ -34,10 +35,15 @@ app.add_url_rule('/diary', 'get_diaries', get_diarys)
 app.add_url_rule('/diary/<int:diary_id>', 'get_diary', get_diary)
 app.add_url_rule('/diary', 'register_diary', register_diary, methods={'POST'})
 
-# diary
+# situation_type
 app.add_url_rule('/situation_type', 'get_situation_type', get_situation_type)
 app.add_url_rule('/situation_type', 'resister_situation_type',
                  resister_situation_type, methods={'POST'})
+
+# topic
+app.add_url_rule('/topic', 'get_topic', get_topics)
+app.add_url_rule('/topic', 'register_topic',
+                 register_topic, methods={'POST'})
 
 # word processing
 app.add_url_rule('/extract-word', 'extract-word',
