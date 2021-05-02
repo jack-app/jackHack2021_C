4f196c0771cbed5ab6eecc4e3cd29ccf122f2d66
shin04 <daikon0413@gmail.com>
2021-05-02 23:16:46 +0900

    check diaryAPI

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,7 @@ from db import db
 from utils.error_handler import handle_bad_request, handle_not_found_request
 from controller.hello import get_hello, post_hello
 from controller.user import get_users, get_user, register_user
+from controller.diary import get_diarys, get_diary, register_diary
 from controller.extract_words import extract_words
 
 app = Flask(__name__)
@@ -18,13 +19,21 @@ app.register_error_handler(400, handle_bad_request)
 app.register_error_handler(404, handle_not_found_request)
 
 # controller
+# sample
 app.add_url_rule('/hello', 'get_hello', get_hello)
 app.add_url_rule('/hello', 'post_hello', post_hello, methods=['POST'])
 
+# users
 app.add_url_rule('/users', 'get_users', get_users)
 app.add_url_rule('/user/<int:user_id>', 'get_user', get_user)
 app.add_url_rule('/user', 'register_user', register_user, methods={'POST'})
 
+# diary
+app.add_url_rule('/diary', 'get_diaries', get_diarys)
+app.add_url_rule('/diary/<int:diary_id>', 'get_diary', get_diary)
+app.add_url_rule('/diary', 'register_diary', register_diary, methods={'POST'})
+
+# word processing
 app.add_url_rule('/extract-word', 'extract-word',
                  extract_words, methods={'POST'})
 
