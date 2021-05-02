62b8b543c59bad643c04edb82168628f38bc55bb
FMVA53E2W\user <youto1223@lookout.jp>
2021-05-02 21:14:35 +0900

    状況と話し相手のタイプのコントローラーを変更しました。

--- backend/controller/situation_type.py
+++ backend/controller/situation_type.py
@@ -4,10 +4,17 @@ from models.situation_type import SituationType
 
 
 def get_situation_type():
-    situ_type = SituationType.get_situation_type()
+    situation_type = SituationType.get_situation_type()
     
     res = []
-    for types in situ_type:
+    for types in situation_type:
         types_dict = types.to_dict
         res.append(types_dict)
     return jsonify(res)
+
+def resister_situation_type():
+    name = request.json["name"]
+    new_situation_type = SituationType(name=name)
+    situation_type_id = new_situation_type.post_situation_type()
+
+    return jsonify({"id": situation_type_id})
