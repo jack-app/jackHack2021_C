d772cb924a492ce830f14d8c4bac17d83b00eb71
FMVA53E2W\user <youto1223@lookout.jp>
2021-05-02 14:02:51 +0900

    状況と話相手のタイプを変更しました

--- backend/controller/situation_type.py
+++ backend/controller/situation_type.py
@@ -4,5 +4,10 @@ from models.situation_type import SituationType
 
 
 def get_situation_type():
-    # TODO: ここから実装
-    pass
+    situ_type = SituationType.get_situation_type()
+    
+    res = []
+    for types in situ_type:
+        types_dict = types.to_dict
+        res.append(types_dict)
+    return jsonify(res)
