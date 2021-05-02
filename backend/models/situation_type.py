193444b201f0f3f0e929510db923cd489b3665ef
FMVA53E2W\user <youto1223@lookout.jp>
2021-05-02 15:12:37 +0900

    話題のモデルを変更しました。

--- backend/models/situation_type.py
+++ backend/models/situation_type.py
@@ -2,13 +2,16 @@ from db.db import db
 
 
 class SituationType(db.Model):
-    message = db.Column(db.String(1024), primary_key=true)
+    id = db.Column(db.Integer, autoincrement=True,primary_key=True)
+    name = db.Column(db.String(1024), primary_key=true)
 
     def to_dict(self):
-        return{'message': self.message,}
+    
+        return{"id": self.id,
+            "name": self.name,}
 
     def __repr__(self):
-        return '<SituationType %r>' % self.message
+        return '<SituationType %r>' % self.name
 
     def get_situation_type():
         situation_type_list = SituationType.query>all()
