4787e72c4c8124928b2b9fcd1ce2b7dbbb55329e
shin04 <daikon0413@gmail.com>
2021-05-02 23:01:05 +0900

    add sql init file

--- backend/db/init_db/2_init.sql
+++ backend/db/init_db/2_init.sql
@@ -4,7 +4,32 @@ CREATE TABLE user
     name VARCHAR(128)
 );
 
-INSERT INTO user
-    (name)
-VALUES
-    ('Bob');
+CREATE TABLE situation_type
+(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(1024)
+);
+
+CREATE TABLE topic
+(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(256),
+    situation_type_id INT
+);
+
+CREATE TABLE question
+(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    content VARCHAR(4096),
+    topic_id INT,
+    situation_type_id INT
+);
+
+CREATE TABLE diary
+(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(128),
+    content VARCHAR(4096),
+    user_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
