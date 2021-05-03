2d13b3bf309c18a4e4f0fa846e85630a72bbd60a
shin04 <daikon0413@gmail.com>
2021-05-03 13:52:15 +0900

    test diary

---
+++ backend/controller/test_user.py
@@ -0,0 +1,23 @@
+import unittest
+import sys
+import io
+import os
+
+from app import app
+
+
+class TestDiary(unittest.TestCase):
+    def setUp(self):
+        print('Test User')
+        app.config['TESTING'] = True
+        self.client = app.test_client()
+
+    def tearDown(self):
+        print('End Test')
+
+    def test_register_diary(self):
+        res = self.client.post('/user', json={})
+        self.assertEqual(res.status_code, 400)
+
+        res = self.client.post('/user', json={'name': ''})
+        self.assertEqual(res.status_code, 400)
