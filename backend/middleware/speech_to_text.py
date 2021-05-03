93d22c4532f81a5b72696dcaa951b2dd52b111cb
shin04 <daikon0413@gmail.com>
2021-05-03 13:09:02 +0900

    wav

--- backend/middleware/speech_to_text.py
+++ backend/middleware/speech_to_text.py
@@ -1,18 +1,18 @@
-from google.cloud import speech_v1p1beta1
+from google.cloud import speech_v1p1beta1 as speech
 
 
 def speech_to_text(voice_file):
-    client = speech_v1p1beta1.SpeechClient()
+    client = speech.SpeechClient()
 
     language_code = "ja-JP"
     sample_rate_hertz = 44100
-    encoding = speech_v1p1beta1.RecognitionConfig.AudioEncoding.MP3
+    # encoding = speech.RecognitionConfig.AudioEncoding.MP3
+    encoding = speech.RecognitionConfig.AudioEncoding.LINEAR16
     config = {
         "language_code": language_code,
         "sample_rate_hertz": sample_rate_hertz,
         "encoding": encoding,
-        "enable_speaker_diarization": True,
-        # "diarization_speaker_count": people_num
+        "enable_speaker_diarization": True
     }
 
     voice_byte = voice_file.read()
