f64885d07885af6cf281bb18f98cf68593d94a1c
teppea14 <tetsusaku14@gmail.com>
2021-05-03 11:25:11 +0900

    録音ページ作りました

--- front/src/container/RecordPage/index.tsx
+++ front/src/container/RecordPage/index.tsx
@@ -1,20 +1,24 @@
 import React, { memo } from 'react';
 import ContentWrapper from "components/atoms/ContentWrapper";
-import { Typography, Grid, Button, makeStyles} from "@material-ui/core";
+import { Typography, Grid, IconButton, makeStyles} from "@material-ui/core";
 import SearchIcon from '@material-ui/icons/Search';
+import MicIcon from '@material-ui/icons/Mic';
 const useStyles = makeStyles((theme) => ({
   button: {
+  width:"150px",
+  height:"150px",
       borderRadius: "",
-      backgroundColor: ""
+      backgroundColor:"red"
   },
   text: {
+      fontSize:"20px",
       color: ""
   },
   expSection: {
-      margin: "px 0"
+      margin: "90px 0"
   },
   buttonSection: {
-      margin: "px 0"
+      margin: "100px 0"
   }
 }));
 
@@ -24,12 +28,13 @@ const RecordPage: React.FC = () => {
         <>
             <ContentWrapper>
                 <div className={classes.expSection}>
-                    <Typography 
+                    <Typography
                         variant="body2"
                         align="left"
                         className={classes.text}>
-                        <br/>
-                        <br/>
+                      使い方<br/>
+                        １相手が話始めたら録音開始<br/>
+                        ２音声認識によって相手の発言に関連するキーワードが表示されます。<br/>
                     </Typography>
                 </div>
                 <div className={classes.expSection}>
@@ -37,9 +42,9 @@ const RecordPage: React.FC = () => {
                         variant="body2"
                         align="left"
                         className={classes.text}>
-                        <SearchIcon/><br/>
-                        <br/>
-                        <br/>
+                        <SearchIcon/>関連キーワード<br/>
+                        お寿司<br/>
+                        お寿司<br/>
                     </Typography>
                 </div>
                 <div className={classes.buttonSection}>
@@ -47,11 +52,11 @@ const RecordPage: React.FC = () => {
                         variant="body1"
                         align="center"
                         className={classes.text}>
-    
+                        録音を開始
                     </Typography>
-                    <Button className={classes.button}>
-    
-                    </Button>
+                    <IconButton className={classes.button}>
+                      <MicIcon/>
+                    </IconButton>
                 </div>
             </ContentWrapper>
         </>
